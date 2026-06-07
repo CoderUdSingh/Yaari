8dabffbb22dde964f77adcc7610eba94f3c59019
CoderUdSingh <uddeshya.professional07@gmail.com>
2026-06-07 19:35:49 +0530

    fix: updated cookie configuration for cross-domain auth

--- apps/api/controllers/usercontroller.js
+++ apps/api/controllers/usercontroller.js
@@ -108,8 +108,8 @@ export const login = async (req, res) => {
     // Packing token in a cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
