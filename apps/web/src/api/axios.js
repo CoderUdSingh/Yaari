640cfc23bf58793f96d441ec5abf93d71fad816e
CoderUdSingh <uddeshya.professional07@gmail.com>
2026-06-07 16:27:14 +0530

    updated with https ec2 domain

--- apps/web/src/api/axios.js
+++ apps/web/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://13.206.221.43.nip.io",
   timeout: 10000,
   withCredentials: true,
 });
